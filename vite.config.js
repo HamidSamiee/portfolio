39a8a99475101eb4194653897db97ee13f299789
HamidSamiee <hamidsmoasers@yahoo.com>
2025-12-03 00:18:36 +0330

    vite update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['portfolio-kjwg.onrender.com']
+  },
+  server: {
+    host: true,
+    port: 3000,
+  }
 })
